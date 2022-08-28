27718617985fb16e470d7f87387f42bfceb8fddf
Shinen1504 <112281857+Shinen1504@users.noreply.github.com>
2022-08-28 09:21:19 +0100

    Add files via upload

---
+++ WEBDEV POE/Index.php
@@ -0,0 +1,24 @@
+
+<html>
+      <head>
+            <title> Ebook Store Home </title>
+      </head>
+
+      <body>
+              <ul>
+
+                    <li> <a href="Index.php"> Home </a> </li>
+                    <li> <a href="About.php"> About </a> </li>
+                    <li> <a href="Books.php"> Books for sale! </a> </li>
+                    <li> <a href="Sell.php"> Sell your books here! </a> </li>
+                    <li> <a href="Login.php"> Login/Signup </a> </li>
+
+             </ul>
+              <h1> Welcome to Ebookstore! </h1>
+    <p>
+Ebook store is an online market place for students that are looking to buy or sell their used textbooks
+    </p>
+
+
+      </body>
+</html>
---
+++ WEBDEV POE/Login.php
@@ -0,0 +1,37 @@
+
+<html>
+      <head>
+            <title> Ebook Store Login </title>
+      </head>
+
+      <body>
+              <ul>
+
+                    <li> <a href="Index.php"> Home </a> </li>
+                    <li> <a href="About.php"> About </a> </li>
+                    <li> <a href="Books.php"> Books for sale! </a> </li>
+                    <li> <a href="Sell.php"> Sell your books here! </a> </li>
+                    <li> <a href="Login.php"> Login/Signup </a> </li>
+
+             </ul>
+              <h1> Login to get started! </h1>
+              <p>  Are you a new student looking to sign up? <a href="Signup.php"> click here</a> </br>
+              for admin login <a href="AdminLogin.php">click here</a> </p>
+<form>
+
+<label for="StudentNumber">Student Number</label>
+<input type="text" id="StudentNumber" name="StudentNumber" placeholder="...">
+</br>
+
+</br>
+<label for="Password">Password</label>
+<input type="text" id="Password" name="Password" placeholder="...">
+
+</br>
+ <input type="submit"> 
+
+</form>
+
+
+      </body>
+</html>
