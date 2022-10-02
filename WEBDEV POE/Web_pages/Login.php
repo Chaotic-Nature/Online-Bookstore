071e93a687d72bd1aac64901844faa5be0dd40ab
Cibrian <hofstacibrian3@gmail.com>
2022-10-02 17:38:14 +0200

    .

--- WEBDEV POE/Web_pages/Login.php
+++ WEBDEV POE/Web_pages/Login.php
@@ -55,15 +55,9 @@
 
                 <input type="text" id="Username" name="Username" class="input-field" placeholder="Enter Username" required>
                 <!-- <input type="text" class="input-field" placeholder="Enter Username" required> -->
-
-<<<<<<< HEAD
                 <input type="email" id="email" name="email" class="input-field" placeholder="Enter Email" required>
                 <!-- <input type="text" class="input-field" placeholder="Enter Username" required> -->
-
-                <input type="text" id="Password" name="Password" class="input-field" placeholder="Enter Password" required>
-=======
                 <input type="Password" id="Password" name="Password" class="input-field" placeholder="Enter Password" required>
->>>>>>> eb655764d10be08e738342c73bdcbabdc3bb9c68
                 <!--<input type="text" class="input-field" placeholder="Enter Password" required>-->
 
                 <input type="Password" id="confPwd" name="confPwd" class="input-field" placeholder="Confirm Password" required>
